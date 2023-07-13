4aba4bf8084f4caece175347047487566118f2a6
Jayesh Batra <bjayesh47@gmail.com>
2023-07-14 01:38:42 +0530

    UI Fix Intro Activity

--- app/src/main/java/in/walnutlabs/teamup/IntroActivity.kt
+++ app/src/main/java/in/walnutlabs/teamup/IntroActivity.kt
@@ -1,8 +1,15 @@
 package `in`.walnutlabs.teamup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Shader.TileMode
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +20,23 @@ class IntroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        setShader(findViewById(R.id.tvTitle))
+        setShader(findViewById(R.id.tvSignUp))
+    }
+
+    private fun setShader(view: TextView) {
+        val viewWidth: Float = view.paint.measureText(view.text as String?)
+
+        val headingShader: LinearGradient = LinearGradient(
+            0F, 0F, viewWidth, view.textSize,
+            intArrayOf(
+                resources.getColor(R.color.app_dark_blue),
+                resources.getColor(R.color.app_light_blue)
+            ),
+            floatArrayOf(0.0F, 1.0F),
+            TileMode.CLAMP)
+
+        view.paint.shader = headingShader
     }
 }
