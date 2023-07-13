0f46aec89242723d10b2dab86b370a3cb9a5f685
Jayesh Batra <bjayesh47@gmail.com>
2023-07-14 00:11:03 +0530

    Added Intro Activity

--- app/src/main/java/in/walnutlabs/teamup/SplashActivity.kt
+++ app/src/main/java/in/walnutlabs/teamup/SplashActivity.kt
@@ -1,6 +1,8 @@
 package `in`.walnutlabs.teamup
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,9 +10,19 @@ class SplashActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this,
+                    IntroActivity::class.java
+                )
+            )
+        }, 1000)
     }
 }
